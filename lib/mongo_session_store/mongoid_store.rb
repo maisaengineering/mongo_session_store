60a422de2aaf97532fe06275c5655ef710b9cc45
Maisa Solutions <contact@maisasolutions.com>
2014-01-22 11:13:04 +0530

    Update mongoid_store.rb

--- lib/mongo_session_store/mongoid_store.rb
+++ lib/mongo_session_store/mongoid_store.rb
@@ -13,12 +13,12 @@ module ActionDispatch
 
         field :_id, :type => String
 
-        field :data, :type => BSON::Binary, :default => BSON::Binary.new(Marshal.dump({}).to_bson,:generic) 
+        field :data, :type => BSON::Binary, :default => BSON::Binary.new({}.to_bson,:generic) 
       end
 
       private
       def pack(data)
-        BSON::Binary.new(Marshal.dump(data).to_bson,:generic)
+        BSON::Binary.new(data.to_bson,:generic)
       end
     end
   end
